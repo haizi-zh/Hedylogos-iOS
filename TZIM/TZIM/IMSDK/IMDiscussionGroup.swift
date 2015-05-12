b05f34a0ec37de8d8ed0253f001bc5359f2d197f
Hacker-liang <pengshuai.liang@gmai.com>
2015-05-12 19:57:20 +0800

    添加讨论组 优化群组 优化好友关系

---
+++ TZIM/TZIM/IMSDK/IMDiscussionGroup.swift
@@ -0,0 +1,13 @@
+//
+//  IMDiscussionGroup.swift
+//  TZIM
+//
+//  Created by liangpengshuai on 5/12/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+import UIKit
+
+class IMDiscussionGroup: NSObject {
+   
+}
