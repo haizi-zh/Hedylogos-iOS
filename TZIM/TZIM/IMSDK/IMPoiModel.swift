5a181ed1eb286ffbe80e809b0cb13a0a498a88df
Hacker-liang <pengshuai.liang@gmai.com>
2015-05-15 16:35:57 +0800

    优化 poi 聊天信息

--- TZIM/TZIM/IMSDK/IMPoiModel.swift
+++ TZIM/TZIM/IMSDK/IMPoiModel.swift
@@ -8,6 +8,27 @@
 
 import UIKit
 
+enum IMPoiType: Int {
+    case City = 0
+    case Guide = 1
+    case Spot = 2
+    case TravelNote = 3
+    case Restaurant = 4
+    case Shopping = 5
+    case Hotel = 6
+}
+
 class IMPoiModel: NSObject {
+    
+    var poiId: String!
+    var poiType: IMPoiType!
+    var poiName: String?
+    var desc: String?
+    var image: String?
+    var address: String?
+    var rating: String?
+    var url: String?
+    var price: String?
+    var timeCost: String?
    
 }
